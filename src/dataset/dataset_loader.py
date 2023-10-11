9e0aaab853a746ed62c4a3a94742d6a1f4b54c69
rileydrizy <rileydrizzy@hotmail.com>
2023-10-11 21:39:10 +0000

    running test in colab

--- src/dataset/dataset_loader.py
+++ src/dataset/dataset_loader.py
@@ -6,13 +6,13 @@ from loguru import logger
 #from utils.logger import logger
 
 url = 'https://www.kaggle.com/competitions/amex-default-prediction/data'
-dir = 'data/raw'
+dir = 'data'
 
 def main():
     logger.info("Commencing the data unzipping process.")
     try:
         opd.download(dataset_id_or_url=url, data_dir= dir)
-
+        logger.info('Data downloaded succesful')
     except Exception as error:
         logger.error("Failed to download")
 
