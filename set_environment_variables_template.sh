87307334b639e0139d5bab30cf0aba7629b53743
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2024-03-05 23:53:53 +0000

    feat(scripts): Update set-enviroment_varaibles_template.sh for Wandb API key

--- set_environment_variables_template.sh
+++ set_environment_variables_template.sh
@@ -5,3 +5,8 @@
 export KAGGLE_USERNAME=username
 export KAGGLE_KEY=xxxxxxxxxxxxxx
 
+#replace with WANDB key
+export WANDB_API_KEY=e05a330f244023f0d49072fe8796f5b1a7026536
+
+
+echo "Enviroment Varibales Set"
